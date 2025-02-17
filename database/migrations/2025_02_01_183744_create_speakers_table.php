d067c555786bea124715dbc02a15ebf3333f3333
ErDekas <pabletor0505@gmail.com>
2025-02-17 11:19:32 +0100

    UPDATE

--- database/migrations/2025_02_01_183744_create_speakers_table.php
+++ database/migrations/2025_02_01_183744_create_speakers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('speakers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('photo_url')->nullable()->default('user2.png');
+            $table->string('photo_url')->nullable()->default('images/user2.webp');
             $table->json('social_links')->nullable();
             $table->json('expertise_areas');
             $table->timestamps();
